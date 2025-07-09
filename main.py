780c01c5242f1ec19aabf41dfe3a086217f3d53b
RishwanthGourishetty <rishwanthgourishetty@teyahealth.com>
2025-07-09 11:16:35 +0530

    Added Two More Http Requests

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/hello/{name}/{age}")
+async def say_hello(name: str, age: int):
+    return {"message": f"Hello {name}, you are {age} years old"}
+
+@app.get("/hello/{name}/{age}/{height}")
+async def say_hello(name: str, age: int, height: float):
+    return {"message": f"Hello {name}, you are {age} years old and {height} meters tall"}
