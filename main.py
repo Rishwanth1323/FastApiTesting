e776bc5674e7bd897d88979978cad0e71d94fabb
RishwanthGourishetty <rishwanthgourishetty@teyahealth.com>
2025-07-09 11:25:53 +0530

    Added ONe more get method with items_id

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
 
